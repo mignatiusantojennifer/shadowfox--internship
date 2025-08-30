6d666d89ae9881a55f8e1142f81657f5e273109b
M.IGNATIUS ANTO JENNIFER <168767795+mignatiusantojennifer@users.noreply.github.com>
2025-08-30 10:49:19 +0530

    TASK-1.py

--- TASK-1.py
+++ TASK-1.py
@@ -39,3 +39,25 @@ print(f"\n🎯 Test Accuracy: {test_acc:.2f}")
 # Step 6: Save model
 model.save("cifar10_cnn_model.h5")
 print("📁 Model saved as cifar10_cnn_model.h5")
+
+
+
+
+
+OUTPUT : 
+
+✅ TensorFlow version: 2.19.0
+Downloading data from https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+170498071/170498071 ━━━━━━━━━━━━━━━━━━━━ 3s 0us/step
+/usr/local/lib/python3.12/dist-packages/keras/src/layers/convolutional/base_conv.py:113: UserWarning: Do not pass an `input_shape`/`input_dim` argument to a layer. When using Sequential models, prefer using an `Input(shape)` object as the first layer in the model instead.
+  super().__init__(activity_regularizer=activity_regularizer, **kwargs)
+Epoch 1/3
+1563/1563 ━━━━━━━━━━━━━━━━━━━━ 66s 41ms/step - accuracy: 0.3999 - loss: 1.6566 - val_accuracy: 0.5935 - val_loss: 1.1576
+Epoch 2/3
+1563/1563 ━━━━━━━━━━━━━━━━━━━━ 62s 39ms/step - accuracy: 0.6139 - loss: 1.1029 - val_accuracy: 0.6437 - val_loss: 1.0169
+Epoch 3/3
+1563/1563 ━━━━━━━━━━━━━━━━━━━━ 63s 40ms/step - accuracy: 0.6741 - loss: 0.9296 - val_accuracy: 0.6747 - val_loss: 0.9359
+WARNING:absl:You are saving your model as an HDF5 file via `model.save()` or `keras.saving.save_model(model)`. This file format is considered legacy. We recommend using instead the native Keras format, e.g. `model.save('my_model.keras')` or `keras.saving.save_model(model, 'my_model.keras')`. 
+
+🎯 Test Accuracy: 0.67
+📁 Model saved as cifar10_cnn_model.h5
